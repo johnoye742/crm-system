83b938ec6729fd3a73ba9ad73a3bef935a8d559c
John Oye <olugbengajohnoye@gmail.com>
2024-05-05 00:26:46 +0100

    Implementing add organizations feature

---
+++ app/Livewire/AddOrganization.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddOrganization extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-organization');
+    }
+}
---
+++ resources/views/livewire/add-organization.blade.php
@@ -0,0 +1,10 @@
+<div class="lg:px-12 px-5 lg:py-14 w-full">
+    <h1>Add New Organization</h1>
+
+    <form class="w-full flex flex-col gap-5">
+        <input type="text" wire:model="name" placeholder="Organization Name" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full lg:w-1/2">
+        <select type="text" wire:model="niche" placeholder="Organization Category" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full lg:w-1/2">
+            <option value="real-estate-management">Real Estate Management</option>
+        </select>
+    </form> 
+</div>
