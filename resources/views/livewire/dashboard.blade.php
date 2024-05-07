3096c66d2f90142ba22031fb3fa03b15538407a6
John Oye <olugbengajohnoye@gmail.com>
2024-05-07 12:37:09 +0100

    Properly set the table for properties, pagination may come in later

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,35 +3,34 @@
         <h1 class="text-2xl">Welcome, {{ $user -> name }}</h1>
 
         <p>{{ $user -> organisation -> name }}</p>
-
-        <h1 class="py-5 text-xl">Properties</h1>
+        <div class="flex flex-row items-center gap-3">
+            <h1 class="py-5 text-xl">Properties</h1>
+            <a href={{ route('add-property') }} class="px-3 py-2 rounded-full text-black drop-shadow-lg w-fit bg-[#DFD0B8]" wire:navigate>Add Property</a>
+        </div>
+        
        
-        <table class="bg-[#3C5B6F] mb-8 table gap-3 rounded-md p-10 overflow-scroll lg:w-[50%] w-full lg:overflow-hidden shadow-2xl">
+        <table class="bg-[#3C5B6F] mb-8 table-auto rounded-md p-10 overflow-scroll border-seperate w-full lg:overflow-hidden shadow-2xl">
             <thead class=" bg-[#948979] px-3 py-2 table-header-group">
-                <tr class="rounded-lg divide-x divide-yellow-400">
+                <tr class="rounded-lg">
                     <th class="px-5 py-2">Property Name</th>
                     <th class="px-5 py-2">Property Price</th>
                     <th class="px-5 py-2 ">Property Location</th>
                 </tr>
             </thead>
 
-            <tbody class="rounded-lg ">
+            <tbody class="rounded-lg">
                 @foreach ($properties as $property)
-                    <tr>
-                        <td>{{ $property -> name }}</td>
-                        <td>{{ $property -> price }}</td>
-                        <td>{{ $property -> location }}</td>
+                    <tr class=" text-white">
+                        <td class="px-5 py-2 border border-slate-800">{{ $property -> property_name  }}</td>
+                        <td class="px-5 border border-slate-800">{{ $property -> property_price }}</td>
+                        <td class="px-5 border border-slate-800">{{ $property -> property_location }}</td>
                     </tr>
                 @endforeach
-                <tr class="divide-x divide-yellow-400 p-5 text-white">
-                    <td class="px-5 py-2">3 bedroom Duplex in the ends</td>
-                    <td class="px-5">$58.00</td>
-                    <td class="px-5">Abuja, Nigeria</td>
-                </tr>
+            
             </tbody>
         </table>
 
-        <a href={{ route('add-property') }} class="mt-5 px-3 py-2 rounded-full text-black drop-shadow-lg w-fit bg-[#DFD0B8]" wire:navigate>Add Property</a>
+       
 
         <h1 class="py-5 text-xl">Clients</h1>
        
