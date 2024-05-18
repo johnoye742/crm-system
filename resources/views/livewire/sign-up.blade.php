5d06f115c7c4c08d95d43da8addbfcdcb9091db6
John Oye <olugbengajohnoye@gmail.com>
2024-05-18 19:57:18 +0100

    The change in sign up

--- resources/views/livewire/sign-up.blade.php
+++ resources/views/livewire/sign-up.blade.php
@@ -11,21 +11,6 @@
             <input type="email" wire:model="form.email" placeholder="Email" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">            
             <input type="text" wire:model="form.fname" placeholder="Full Name" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">
             
-            <!-- Select Role -->
-            @error('form.role')
-                <p>Please select a Role</p>
-            @enderror
-            <div class="w-full flex flex-row items-center gap-2">
-                <label for="role">Role: </label>
-                <select id="role" class="p-3 py-2 rounded-full" wire:model="form.role">
-                    <option>-- Select a role --</option>
-                    <option value="agent">Agent</option>
-                </select>
-            </div>
-            <!-- Select Role -->
-            @error('form.role')
-                <p>Please select a Role</p>
-            @enderror
             <div class="w-full flex flex-row items-center gap-2">
                 <label for="niche">Business Type: </label>
                 <select id="niche" class="p-3 py-2 rounded-full" wire:model="form.niche">
@@ -34,18 +19,8 @@
                 </select>                                                       
                                    
             </div>  
-
-            <div class="w-full flex flex-row items-center gap-2">
-                <label for="org">Organization Name: </label>
-                <select id="org" class="p-3 py-2 rounded-full" wire:model="form.org">
-                    <option>-- Select an organisation --</option>
-                    @foreach ($organizations as $org)
-                        <option value="{{ $org -> id }}">{{ $org -> name }}</option>
-                    @endforeach                                        
-                </select>                
-            </div>  
-
                                                                                           
+            <input type="organisation_name" wire:model="form.org_name" placeholder="Organisation name" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">
             <input type="password" wire:model="form.pwd" placeholder="Password" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">
             
             @error('form.pwd2') <p>Passwords do not match</p> @enderror                                                                               
