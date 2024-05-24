96de2e4fcabb123b94557554f3a5b55fe1e6de01
John Oye <olugbengajohnoye@gmail.com>
2024-05-24 20:10:49 +0100

    Read the file name

---
+++ app/Http/Middleware/EnsureAdminOrAgent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAdminOrAgent
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(strtolower($request -> user() -> role) != 'admin' || $request -> user() -> role != 'real-estate-agent')
+            return redirect() -> route('dashboard');
+        
+        return $next($request);
+    }
+}
