621c0f0fe5390711a3050c2c9c3c5773211ccfb1
John Oye <olugbengajohnoye@gmail.com>
2024-06-08 02:53:23 +0100

    JS wahala

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ __webpack_require__.r(__webpack_exports__);
   \*****************************/
 /***/ (() => {
 
-throw new Error("Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\nFile was processed with these loaders:\n * ./node_modules/babel-loader/lib/index.js\nYou may need an additional loader to handle the result of these loaders.\n> import './bootstrap';");
+
 
 /***/ })
 
