495812bddecbe6955dc6574926b3ac7e1d0f0e4e
John Oye <olugbengajohnoye@gmail.com>
2024-06-08 02:52:22 +0100

    Configured to allow https connections

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Allow https requests in production
+        if(env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
         // Prevent any non-administrator from adding employees to an organisation
         Gate::define('add-employees', function (User $user) {
 
