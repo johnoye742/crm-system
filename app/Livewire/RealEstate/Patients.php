9bef1eaed6b4efdeb0162c02d66f10a3b2ecf008
John Oye <olugbengajohnoye@gmail.com>
2024-07-11 23:18:44 +0100

    Setup for Patients page

---
+++ app/Livewire/RealEstate/Patients.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\RealEstate;
+
+use Livewire\Component;
+
+class Patients extends Component
+{
+    public function render()
+    {
+        return view('livewire.real-estate.patients');
+    }
+}
