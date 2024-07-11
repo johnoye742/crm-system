974e9ef3ff49f5a4de7984fef19c4bb495e31324
John Oye <olugbengajohnoye@gmail.com>
2024-07-11 23:23:18 +0100

    Arranged the layout structure

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,8 +9,27 @@
         <link rel="stylesheet" href={{ asset('css/app.css') }}>
         <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
     </head>
-    <body>
+    <body class="w-full flex flex-row">
+        @auth
+
         <livewire:header></livewire:header>
-        {{ $slot }}
+        <div class="lg:ml-[25%] ml-[10%] w-full">
+            <div class="w-full flex flex-row justify-between">
+                <div class="p-5 lg:px-12 flex flex-row items-center">
+                    <h1 class="text-2xl">Welcome, {{ request() -> user() -> name }}</h1>
+                </div>
+
+                <div class="flex flex-row p-5 items-center gap-2">
+                    <img class="w-[2em] h-[2em]" src="https://avatar.iran.liara.run/public/job/doctor/male?username=johnoye742">
+                    <div class="flex flex-col">
+                        <p>{{ request() -> user() -> name }}</p>
+                        <p>{{ request() -> user() -> email }}</p>
+                    </div>
+                </div>
+        </div>
+        @endauth
+
+            {{ $slot }}
+        </div>
     </body>
 </html>
