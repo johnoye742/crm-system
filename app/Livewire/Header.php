b50d3209361aabf4d0d966c233516897978f1917
John Oye <olugbengajohnoye@gmail.com>
2024-07-11 23:19:54 +0100

    Done page navigation design for the header

--- app/Livewire/Header.php
+++ app/Livewire/Header.php
@@ -3,14 +3,30 @@
 namespace App\Livewire;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Livewire\Component;
 
 class Header extends Component
 {
     public $showNav;
+    public $page;
 
     public function mount() {
         $showNav = false;
+
+        switch (request() -> url()) {
+            case route('dashboard'):
+                $this -> page = 'dashboard';
+                break;
+
+            case route('employees'):
+                $this -> page = 'employees';
+                break;
+
+            default:
+                $this->page = 'dashboard';
+                break;
+        }
     }
 
     public function show() {
@@ -23,6 +39,9 @@ class Header extends Component
 
     public function render()
     {
+        if(request() -> user() -> niche == 'health-care') {
+            return view('livewire.health-care.header');
+        }
         return view('livewire.real-estate.header');
     }
 
