f51816d0ed8f4aeb4302f7286a4ce18edd595e5e
John Oye <olugbengajohnoye@gmail.com>
2024-07-13 12:41:59 +0100

    Captialize genders

--- resources/views/livewire/health-care/dashboard.blade.php
+++ resources/views/livewire/health-care/dashboard.blade.php
@@ -29,8 +29,8 @@
                         @foreach ($patients as $patient)
                             <tr class=" text-white">
                                 <td class="px-5 py-2 border  overflow-scroll lg:overflow-auto">{{ $patient -> name  }}</td>
-                                <td class="px-5 border  overflow-scroll lg:overflow-auto">{{ $patient -> dob }}</td>
-                                <td class="px-5 border  overflow-scroll lg:overflow-auto">{{ $patient -> gender }}</td>
+                                <td class="px-5 border capitalize overflow-scroll lg:overflow-auto">{{ $patient -> dob }}</td>
+                                <td class="px-5 border capitalize overflow-scroll lg:overflow-auto">{{ $patient -> gender }}</td>
                                 <td class="px-5 border  overflow-scroll lg:overflow-auto">{{ $patient -> state_of_origin }}</td>
                                 <td class="px-5 border  overflow-scroll lg:overflow-auto">{{ $patient -> occupation }}</td>
 
@@ -45,7 +45,7 @@
                     </tbody>
                 </table>
 
-                <a href={{ route('health-care.add-patient') }} class="px-3 py-2 rounded-full text-black drop-shadow-lg w-fit bg-[#DFD0B8]" wire:navigate>View All</a>
+                <a href={{ route('health-care.patients') }} class="px-3 py-2 rounded-full text-black drop-shadow-lg w-fit bg-[#DFD0B8]" wire:navigate>View All</a>
 
             @else
                 <p>No Available properties</p>
