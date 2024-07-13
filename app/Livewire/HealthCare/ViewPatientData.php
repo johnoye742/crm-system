89eeb0c6f0e2fa31fe386774d5230776e025eb2c
John Oye <olugbengajohnoye@gmail.com>
2024-07-13 12:41:03 +0100

    Update ViewPatientData.php
    Made it cleaner

--- app/Livewire/HealthCare/ViewPatientData.php
+++ app/Livewire/HealthCare/ViewPatientData.php
@@ -4,8 +4,7 @@ namespace App\Livewire\HealthCare;
 
 use App\Models\Patient;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class ViewPatientData extends Component
@@ -13,15 +12,15 @@ class ViewPatientData extends Component
     public $patient;
 
     public function mount(User $user, Patient $patient) {
-        
+
     }
 
-    public function render()
+    public function render() : View
     {
         $patient = $this -> patient;
         return view('livewire.health-care.view-patient-data', [
             "patient" => $patient,
-            "emergency_contact" => json_decode(str_replace(" ", "", $patient -> emergency_contact), true),
+            "emergency_contact" =>  $patient -> emergency_contact,
             "medical_record" => $patient -> records
         ]) -> title("View Patient - ". $patient -> name);
     }
