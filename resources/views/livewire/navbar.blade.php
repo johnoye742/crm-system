80dc3f4251d04e7d0a0b922d196342c4e93bc05e
John Oye <olugbengajohnoye@gmail.com>
2024-07-31 00:09:24 +0100

    Create navbar.blade.php

---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,7 @@
+<div class="w-full top-0 left-0 text-white flex flex-row justify-between px-5 py-5 bg-[#153448]">
+    <h1>CRMStar</h1>
+
+    <ul>
+        <li><a class="" href={{ route('homepage') }}>Home</a></li>
+    </ul>
+</div>
