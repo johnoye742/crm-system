efabce98c8da9c6ad61560280c4cc90b76bf2dff
John Oye <olugbengajohnoye@gmail.com>
2024-07-31 00:14:50 +0100

    Create Sales.php

---
+++ app/Livewire/RealEstate/Sales.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\RealEstate;
+
+use Livewire\Component;
+
+class Sales extends Component
+{
+    public function render()
+    {
+        return view('livewire.real-estate.sales');
+    }
+}
