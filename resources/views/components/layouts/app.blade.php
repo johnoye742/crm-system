1722e039416171384291eedb4a761ec1964c91c0
John Oye <olugbengajohnoye@gmail.com>
2024-07-31 00:14:17 +0100

    Reworked the layout to fit current application needs

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,25 +8,30 @@
 
         <link rel="stylesheet" href={{ asset('css/app.css') }}>
         <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+
+            <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.0/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+        <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.2/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     </head>
-    <body class="w-full flex flex-row">
+    <body class="w-full flex @auth flex-row @else flex-col @endauth">
         @auth
 
-        <livewire:header></livewire:header>
-        <div class="lg:ml-[25%] ml-[10%] w-full">
-            <div class="w-full flex flex-row justify-between">
-                <div class="p-5 lg:px-12 flex flex-row items-center">
-                    <h1 class="text-2xl">Welcome, {{ request() -> user() -> name }}</h1>
-                </div>
-
-                <div class="flex flex-row p-5 items-center gap-2">
-                    <img class="w-[2em] h-[2em]" src="https://avatar.iran.liara.run/public/job/doctor/male?username=johnoye742">
-                    <div class="flex flex-col">
-                        <p>{{ request() -> user() -> name }}</p>
-                        <p>{{ request() -> user() -> email }}</p>
+            <livewire:header></livewire:header>
+            <div class="lg:ml-[25%] ml-[10%] w-full">
+                <div class="w-full flex flex-row justify-between">
+                    <div class="p-5 lg:px-12 flex flex-row items-center">
+                        <h1 class="text-2xl">Welcome, {{ request() -> user() -> name }}</h1>
                     </div>
-                </div>
-        </div>
+
+                    <div class="flex flex-row p-5 items-center gap-2">
+                        <img class="w-[2em] h-[2em]" src="https://avatar.iran.liara.run/public/job/doctor/male?username=johnoye742">
+                        <div class="flex flex-col">
+                            <p>{{ request() -> user() -> name }}</p>
+                            <p>{{ request() -> user() -> email }}</p>
+                        </div>
+                    </div>
+            </div>
+        @else
+            <livewire:navbar></livewire:navbar>
         @endauth
 
             {{ $slot }}
