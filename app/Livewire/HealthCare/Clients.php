f972704b4352f4d458744e2879ed32b8356344f3
John Oye <olugbengajohnoye@gmail.com>
2024-07-31 00:15:26 +0100

    Create Clients.php

---
+++ app/Livewire/HealthCare/Clients.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HealthCare;
+
+use Livewire\Component;
+
+class Clients extends Component
+{
+    public function render()
+    {
+        return view('livewire.health-care.clients');
+    }
+}
