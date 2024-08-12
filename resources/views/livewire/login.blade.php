7b3ce062a6200855dddbbe67f57c4f3a13e778b8
John Oye <olugbengajohnoye@gmail.com>
2024-08-13 00:20:08 +0100

    Update login.blade.php

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -5,10 +5,11 @@
     <div class="flex flex-col lg:mt-32 mt-20">
         <form class="flex flex-col gap-3" wire:submit="login">
             <h1 class=" text-3xl uppercase">Login</h1>
-            <input type="email" wire:model="email" placeholder="Email" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">            
+            <input type="email" wire:model="email" placeholder="Email" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">
             <input type="password" wire:model="pwd" placeholder="Password" class="bg-gray-200 focus:border-[#3C5B6F] border-gray-200 border-2 outline-none px-3 py-2 rounded-full transition-all duration-150 ease-in-out w-full">
+    <a href="{{ route('password.request') }}" class="text-[#DFD0B8]" wire:navigate>Forgot Password</a>
             <button type="submit" href={{ route('register') }} class=" px-3 py-2 rounded-full text-white w-fit bg-[#3C5B6F]">Login</button>
             <p>Don't have an account? <a href="{{ route('register') }}" class="text-[#DFD0B8]" wire:navigate>Register</a></p>
-        </form>    
+        </form>
     </div>
-</div> 
+</div>
