54dd52cbd098474821bb2709fa0bc800303d97ae
John Oye <olugbengajohnoye@gmail.com>
2024-09-05 11:38:27 +0100

    Testing the project hermes

--- app/Livewire/HealthCare/Patients.php
+++ app/Livewire/HealthCare/Patients.php
@@ -6,6 +6,7 @@ use App\Models\Organisation;
 use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Johnoye742\PhpHermes\Hermes;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -15,6 +16,10 @@ class Patients extends Component
     public function render()
     {
         $patients = User::where('organisation_id', Auth::user() -> organisation -> id) -> where('role', 'health-care-patient') -> orderBy('id', 'DESC') -> paginate(15);
+
+        $hermes = new Hermes('localhost', 2907);
+        $hermes -> set('patients:'. Auth::user() -> organisation_id, $patients -> get());
+
         return view('livewire.health-care.patients', ['patients' => $patients]);
     }
 }
