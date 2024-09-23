ac14cdd811cb8bdb8d8245fc63217b5fddbc3145
John Oye <olugbengajohnoye@gmail.com>
2024-09-23 13:03:21 +0100

    Did some changes to the default dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,14 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Organisation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Dashboard')]
 class Dashboard extends Component
 {
+    public $organisations = [];
     public function render()
     {
+        $orgs = Auth::user() -> organisations;
+        if($orgs != null) {
+            foreach ($orgs as $org) {
+                array_push($this -> organisations, Organisation::find($org));
+            }
+        }
+        Log::debug($this -> organisations);
         return view('livewire.dashboard');
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,14 @@
 <div class="px-12">
-    @if (request() -> user() -> organisations == null)
+    @if ($organisations == null)
         <section class="flex-col flex gap-3">
             <h1 class="text-4xl">No organisations</h1>
             <p>You belong to no organisations, consider requesting the admin of an organisation to add you to one, or creating a new one.</p>
             <a href={{ route('organisations.new') }} class=" px-3 py-2 rounded-full text-white w-fit bg-[#3C5B6F]">+ Create</a>
+        </section>
+
+    @else
+        <section>
+
         </section>
     @endif
 </div>
