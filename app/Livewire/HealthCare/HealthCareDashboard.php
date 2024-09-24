05df9409eb90d78c90e3a742542db9f7d3eef962
John Oye <olugbengajohnoye@gmail.com>
2024-09-24 06:04:09 +0100

    Changed the logic for getting the user_organisation

--- app/Livewire/HealthCare/HealthCareDashboard.php
+++ app/Livewire/HealthCare/HealthCareDashboard.php
@@ -6,6 +6,7 @@ use App\Models\Organisation;
 use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -15,9 +16,8 @@ class HealthCareDashboard extends Component
     public function render()
     {
         $user = Auth::user();
-        $user_organisation = $user -> organisation;
+        $user_organisation = Organisation::find($user -> current_organisation);
         $patients = User::where('organisation_id', $user_organisation -> id) -> where('role', 'health-care-patient') -> orderBy('id', 'DESC') -> limit(5) -> get();
-
         return view('livewire.health-care.dashboard', [
             'user' => $user,
             'patients' => $patients,
