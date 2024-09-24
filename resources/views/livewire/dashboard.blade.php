0f25a8f7784b66f9d29f7444c78a1238434446bd
John Oye <olugbengajohnoye@gmail.com>
2024-09-24 06:00:56 +0100

    Show different dashboard depending on the niche
    If a user is signed in to an organisation with health-care niche it shows the dashboard view for that

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,14 +1,13 @@
-<div class="px-12">
+<div >
     @if ($organisations == null)
-        <section class="flex-col flex gap-3">
+        <section class="flex-col flex gap-3 px-12">
             <h1 class="text-4xl">No organisations</h1>
             <p>You belong to no organisations, consider requesting the admin of an organisation to add you to one, or creating a new one.</p>
             <a href={{ route('organisations.new') }} class=" px-3 py-2 rounded-full text-white w-fit bg-[#3C5B6F]">+ Create</a>
         </section>
+    @endif
 
-    @else
-        <section>
-
-        </section>
+    @if($current_org != null && $current_org -> niche == "health-care")
+        <livewire:health-care.health-care-dashboard />
     @endif
 </div>
